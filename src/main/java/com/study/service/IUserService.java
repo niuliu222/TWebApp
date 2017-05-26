b6573cffbbf3e7900aef77b40db39c7b8ad9b886
tyler <ascomtohom@126.com>
2017-05-26 22:08:55 +0800

    004 整合spring,springMVC,mybatis（重构）

---
+++ src/main/java/com/study/service/IUserService.java
@@ -0,0 +1,9 @@
+package com.study.service;
+
+import com.study.model.User;
+
+public interface IUserService {
+
+	public User getUserById(String id);
+	
+}
--- src/test/java/com/study/test/TestUser.java
+++ src/test/java/com/study/test/TestUser.java
@@ -1,5 +1,28 @@
 package com.study.test;
 
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.study.model.User;
+import com.study.service.IUserService;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "classpath:springmvc.xml")
 public class TestUser {
 
+    @Autowired
+    IUserService userService;
+    
+    @Test
+    public void TestGetUser()
+    {
+        User user = userService.getUserById("1");
+        assertTrue(user.getName().equals("student1"));
+        System.out.println(user.getName());
+    }
 }
